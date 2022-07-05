b9d504c2d37fa6d8481178d75044d2838696288b
lolg69 <69385870+lolg69@users.noreply.github.com>
2022-07-05 11:55:34 +0530

    Add files via upload

---
+++ typeCaster.java
@@ -0,0 +1,9 @@
+class typeCaster{
+	public static void main(String args[]){
+	char let = 'G';
+	int ascii = let;
+	char lol = (char)ascii;
+	System.out.println(ascii);
+	System.out.println(lol);		
+	}
+}
