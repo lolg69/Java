20f38eb191a37506f73392316faebaf875c0d166
Mayur Kalsekar <69385870+lolg69@users.noreply.github.com>
2022-07-26 11:28:50 +0530

    Update StringsAndObjects.java

--- StringsAndObjects.java
+++ StringsAndObjects.java
@@ -7,12 +7,22 @@ class StringsAndObjects{
 		}else{
 			System.out.println("NotSame 1");
 		}
-		String s = new String("Ravi");
+		String s = new String("Mayur");
 		String s1 = new String("Dahi");
 		if (s.equals(s1)){
 			System.out.println("Same 2");
 		}else{
 			System.out.println("NotSame 2");
+		}
+		if (fname == s){
+			System.out.println("Same 3");
+		}else{
+			System.out.println("NotSame 3");
+		}
+		if (s.equals(fname)){
+			System.out.println("Same 4");
+		}else{
+			System.out.println("NotSame 4");
 		}
 	}
 }
